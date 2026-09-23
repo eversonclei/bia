52f7ee6b5df89c9b3d996c3c772b4a379b472326
Everson Clei <everson.clei@gmail.com>
2026-09-23 03:58:50 +0000

    deploy com cicd

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add Task - ALB
+        Add Task com CI-CD 
       </button>
       
       <Modal
